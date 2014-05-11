0b20f05c22f5bf25eb4c551b3014e1f8908dfab1
fei <aric@arics.(none)>
2014-05-11 10:38:49 +0800

    测试用例调试成功

---
+++ src/main/java/org/apache/commons/pool2/sample/ReaderUtil.java
@@ -0,0 +1,50 @@
+package org.apache.commons.pool2.sample;
+
+import org.apache.commons.pool2.ObjectPool;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * Maintains a pool of StringBuffers used to dump contents of Readers.
+ */
+public class ReaderUtil {
+
+    private ObjectPool<StringBuffer> pool;
+
+    public ReaderUtil(ObjectPool<StringBuffer> pool) {
+        this.pool = pool;
+    }
+
+    /**
+     * Dumps the contents of the {@link java.io.Reader} to a String, closing the {@link java.io.Reader} when done.
+     */
+    public String readToString(Reader in)
+            throws IOException {
+        StringBuffer buf = null;
+        try {
+            buf = pool.borrowObject();
+            for (int c = in.read(); c != -1; c = in.read()) {
+                buf.append((char) c);
+            }
+            return buf.toString();
+        } catch (IOException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to borrow buffer from pool" + e.toString());
+        } finally {
+            try {
+                in.close();
+            } catch (Exception e) {
+                // ignored
+            }
+            try {
+                if (null != buf) {
+                    pool.returnObject(buf);
+                }
+            } catch (Exception e) {
+                // ignored
+            }
+        }
+    }
+}
---
+++ src/main/java/org/apache/commons/pool2/sample/ReaderUtilClient.java
@@ -0,0 +1,24 @@
+package org.apache.commons.pool2.sample;
+
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+/**
+ * Instantiates and uses a ReaderUtil. The GenericObjectPool supplied to the constructor will have
+ * default configuration properties.
+ */
+public class ReaderUtilClient {
+
+    public static void main(String[] args) {
+        ReaderUtil readerUtil = new ReaderUtil(new GenericObjectPool<StringBuffer>(new StringBufferFactory()));
+        Reader reader = new StringReader("foo");
+        try {
+            System.out.println(readerUtil.readToString(reader));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
